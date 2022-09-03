254932c1172d4e9fb42d6e505d9e15865e17fea4
vishuwas <vishwas1415@gmail.com>
2022-09-03 19:26:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+// import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+
+  constructor (props) {
+    super (props);
+    this.state = {
+      variable : 5
+    }
+    this.abc = this.abc.bind(this);
+    this.xyz = this.xyz.bind(this);
+  }
+
+  abc() {
+    this.setState({
+      variable : ++this.state.variable
+    })
+  }
+
+  xyz() {
+    this.setState({
+      variable : --this.state.variable
+    })
+  }
+
+  render () {
+    return (
+      <>
+    <button onClick={() => this.abc()}>Incremental</button>
+    {this.state.variable}<br />
+    <button onClick={() => this.xyz()}>Decrement</button>    
+    </>
+    )
+  }
 }
-
-export default App;
